2118cbb07c3fdeee529580726f07faa7a64ef7af
Chris Winter <corvusnebula@gmail.com>
2020-01-04 23:16:20 +0100

    added a mongodb

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ var bodyParser = require('body-parser')
 var cors = require('cors')
 var _ = require('lodash')
 
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+// connection url
+const url = 'mongodb://localhost:27017';
+// database Name
+const dbName = 'smartbrainDB';
+// use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  client.close();
+});
+
 const database = {
   users: [{
     id: '1',
